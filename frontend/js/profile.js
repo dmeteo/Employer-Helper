f9c8bb443ec36a05bd5a691dba803ec815be2301
Lehinskiy05 <peskovatskiy.leha@mail.ru>
2024-11-09 13:10:42 +0500

    frontend - profile page

---
+++ frontend/js/profile.js
@@ -0,0 +1,37 @@
+profileInfoForm = document.querySelector('.profile-info');
+editInfoButton = document.querySelector('.profile-info-edit-button');
+canselInfoButton = document.querySelector('.profile-info-cansel-button');
+saveInfoButton = document.querySelector('.profile-info-save-button');
+profileInfoInputs = document.querySelectorAll('.profile-info-input');
+
+editInfoButton.addEventListener('click', function() {
+    for (let i = 0; i < profileInfoInputs.length; i++) {
+        profileInfoInputs[i].disabled = false;
+    }
+    editInfoButton.hidden = true;
+    canselInfoButton.hidden = false;
+    saveInfoButton.hidden = false;
+});
+
+canselInfoButton.addEventListener('click', function() {
+    /* Cancel changes */
+    for (let i = 0; i < profileInfoInputs.length; i++) {
+        profileInfoInputs[i].disabled = true;
+    }
+    editInfoButton.hidden = false;
+    canselInfoButton.hidden = true;
+    saveInfoButton.hidden = true;
+});
+
+saveInfoButton.addEventListener('click', function() {
+    for (let i = 0; i < profileInfoInputs.length; i++) {
+        profileInfoInputs[i].disabled = true;
+    }
+    editInfoButton.hidden = false;
+    canselInfoButton.hidden = true;
+    saveInfoButton.hidden = true;
+});
+
+profileInfoForm.addEventListener('submit', function(evt) {
+    /* Save changes */
+})
